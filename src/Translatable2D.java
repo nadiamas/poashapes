ff13c3eb906aec6ad3dfcc1d08edb5a12279b432
nadia <nadia.maslouhi@gmail.com>
2016-10-02 23:12:48 +0200

    ADD INTERFACE TRANSLATABLE2D AND TRANSLATABLE3D

---
+++ src/Translatable2D.java
@@ -0,0 +1,8 @@
+/**
+ * Created by nadia on 02/10/2016.
+ */
+public interface Translatable2D {
+
+    public abstract Point2D getRefPoint();
+
+}
---
+++ src/Translatable3D.java
@@ -0,0 +1,8 @@
+/**
+ * Created by nadia on 02/10/2016.
+ */
+public interface Translatable3D extends Translatable2D {
+
+    public abstract Point3D getRefPoint();
+
+}
